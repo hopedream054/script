8bf418f939b9b310f3345c1e7330eab0b8d7abb3
hopedream054 <hopedream054@naver.com>
2017-06-14 18:08:57 +0900

    save

--- team/service.py
+++ team/service.py
@@ -15,6 +15,8 @@ class SERVICE:
         pass
 
     def Search(Moviename):
+        fLetter = open("letter.txt", "w")
+        fLetter.write("영화 제목 : "+Moviename+'\n')
         encText = urllib.parse.quote(Moviename)
         key = '8be8eb002379e48966e30456fc238974'
         url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.xml?" + "key=" + key + "&movieNm=" + encText
@@ -37,10 +39,13 @@ class SERVICE:
                 print(a.findtext('movieNm'))
                 # cc="영화 개봉일자 : "+ a.findtext('openDt')[0:4]+"년 "+a.findtext('openDt')[4:6]+"월 "+a.findtext('openDt')[6:8]"일"
                 cc = a.findtext('openDt')
+
                 dayz = "영화의 개봉일자  : " + cc[0:4] + "년 " + cc[4:6] + "월 " + cc[6:8] + "일"
                 # print( + ( + "년 "+ a.findtext('openDt')[4:6] + "월 " ++ a.findtext('openDt')[6:8]+"일"))
                 print(dayz)
+                fLetter.write(dayz + '\n')
                 print("영화의 장르 : ", a.findtext('genreAlt'))
+                fLetter.write("영화의 장르 : "+ a.findtext('genreAlt') + '\n')
 
                 data = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.xml?" + "key=" + key + "&movieCd=" + a.findtext(
                     'movieCd')
@@ -52,19 +57,29 @@ class SERVICE:
                 treeT = etree.parse("threeSample.xml")
                 rootT = treeT.getroot()
 
-                print("상영시간")
-                for cc in rootT.iter("movieInfo"):
-                    print("    " + cc.findtext("showTm") + "분")
 
-                print("영화감독")
+                for cc in rootT.iter("movieInfo"):
+                    if (aa.findtext("showTm") != None):
+                        print("상영시간")
+                        print("    " + cc.findtext("showTm") + "분")
+                        fLetter.write("상영시간" + '\n' + "    " + cc.findtext("showTm") + "분" + '\n')
+
+
                 for aa in rootT.iter("director"):
-                    print("    " + aa.findtext("peopleNm"))
-                print("영화배우")
+                    if (aa.findtext("peopleNm") != None):
+                        print("영화감독")
+                        print("    " + aa.findtext("peopleNm"))
+                        fLetter.write("영화감독" + '\n' + "    " + aa.findtext("peopleNm") + '\n')
+
                 for aa in rootT.iter("actor"):
-                    print("    " + aa.findtext("peopleNm"))
+                    if(aa.findtext("peopleNm")!=None):
+                        print("영화배우")
+                        print("    " + aa.findtext("peopleNm"))
+                        fLetter.write("영화배우"+ '\n' + "    " + aa.findtext("peopleNm") + '\n')
                 print('----------------------')
         else:
             print("Error Code:" + rescode)
+        fLetter.close()
 
     def Seach(Moviename):
         client_id = "2KUjl0QjQdmVCG7JbOW2"
@@ -83,6 +98,7 @@ class SERVICE:
         f = open("sample.xml", "wb")
         f.write(data)
         f.close()
+
 
 
         rescode = response.getcode()
@@ -114,8 +130,12 @@ class SERVICE:
             print("Error Code:" + rescode)
 
     def bestMovieDay(day):
-        print("오늘의 박스오피스!")
 
+        fLetter = open("letter.txt", "w")
+
+
+        print("오늘의 박스오피스!")
+        fLetter.write("오늘의 박스오피스!\n\n")
 
         key = '8be8eb002379e48966e30456fc238974'
         rankXml = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml?" + "key=" + key + "&targetDt=" + day
@@ -138,6 +158,13 @@ class SERVICE:
             print("국내 누적 관객 수 : " + a.findtext('audiAcc'))
             print("-----")
 
+            fLetter.write("영화 제목 : " + a.findtext('movieNm')+'\n')
+            fLetter.write("순위 : " + a.findtext('rank')+'\n')
+            fLetter.write("전날 대비 순위 변동 폭 : " + a.findtext('rankInten')+'\n')
+            fLetter.write("개봉 날짜 : " + a.findtext('openDt')+'\n')
+            fLetter.write("국내 누적 관객 수 : " + a.findtext('audiAcc')+'\n')
+            fLetter.write("-----"+'\n')
+        fLetter.close()
     def bestMovieWantDay(day):
         print(day[0:4]+"년"+day[4:6]+"월"+day[6:8]+"일"+" 박스오피스!"+'\n')
 
